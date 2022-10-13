04b55a0109ee377d3a47dcd5f6a76efdcc68c832
Ali Shirani <alishiranii1384@gmail.com>
2022-10-13 17:55:00 +0330

    FIXED : light box

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const closeBtn = document.querySelector(".close-btn");
 const menu = document.querySelector(".nav_links");
 const overlay = document.querySelector(".overlay");
 const mainThumbnail = document.querySelector(".main-thumbnail");
+const mainThumbnailLightBox = document.querySelector(
+  ".lightbox-container .main-thumbnail"
+);
 const images = document.querySelectorAll(".preview img");
 const plusBtn = document.querySelector("#plus");
 const minusBtn = document.querySelector("#minus");
@@ -12,7 +15,9 @@ const prevBtn = document.getElementById("previous");
 const slider = document.querySelector(".mobile-thumb");
 const thumbMob = document.querySelector(".thumb-mob");
 const cartBtn = document.querySelector(".cart-btn");
-const cart=document.querySelector(".cart-wrp")
+const cart = document.querySelector(".cart-wrp");
+const closeLightboxBtn = document.querySelector(".close-lightbox");
+const lightbox = document.querySelector(".lightbox");
 let amountValue = 0;
 let img = 1;
 
@@ -53,6 +58,12 @@ function prevImage() {
 function toggleCart() {
     cart.classList.toggle("invisible");
 }
+function closeLightBox() {
+    lightbox.classList.add("invisible")
+}
+function openLightBox() {
+    lightbox.classList.remove("invisible")
+}
 
 images.forEach((image) => {
     image.addEventListener("click", () => {
@@ -64,16 +75,20 @@ images.forEach((image) => {
         const selectedImg = document.querySelector(".selected");
         switch (selectedImg.getAttribute("src")) {
           case "./images/image-product-1-thumbnail.jpg":
-            mainThumbnail.src = "./images/image-product-1.jpg";
+                mainThumbnail.src = "./images/image-product-1.jpg";
+                mainThumbnailLightBox.src = "./images/image-product-1.jpg";
             break;
             case "./images/image-product-2-thumbnail.jpg":
                 mainThumbnail.src = "./images/image-product-2.jpg";
+                mainThumbnailLightBox.src = "./images/image-product-2.jpg";
                 break;
             case "./images/image-product-3-thumbnail.jpg":
                 mainThumbnail.src = "./images/image-product-3.jpg";
+                mainThumbnailLightBox.src = "./images/image-product-3.jpg";
                 break;
             case "./images/image-product-4-thumbnail.jpg":
                 mainThumbnail.src = "./images/image-product-4.jpg";
+                mainThumbnailLightBox.src = "./images/image-product-4.jpg";
                 break;
         }
         
@@ -88,3 +103,5 @@ minusBtn.addEventListener("click", handleMinus);
 nextBtn.addEventListener("click", nextImage);
 prevBtn.addEventListener("click", prevImage);
 cartBtn.addEventListener("click", toggleCart);
+closeLightboxBtn.addEventListener("click", closeLightBox);
+mainThumbnail.addEventListener("click",openLightBox)
